bcf3ac23c278912197e621b346affeabb785ac83
Adrien MARION <adrien.marion@epitech.eu>
2021-03-12 18:31:32 +0100

    Initial commit

---
+++ resources/js/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                Bonjour {this.props.name}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Main name="Adrimarn"/>,
+    document.getElementById('index')
+);
